080ef4f77ddce5e519ffc6f1952f3891d3289228
León Orell Valerian Liehr <liehr.exchange@gmx.net>
2020-12-08 22:57:37 +0100

    Change GitHub URL
    The previous one did not work for me

--- preprocessing/cloner.py
+++ preprocessing/cloner.py
@@ -15,8 +15,8 @@ class Cloner:
 
         If the project already exists at the destination location, we replace it by the new clone
         """
-        repo_url = "git@github.com:" + author + "/" + repo + ".git"
-        destination_path = os.path.join(destination, author + "__" + repo)
+        repo_url = f"https://github.com/{author}/{repo}.git"
+        destination_path = os.path.join(destination, f"{author}__{repo}")
 
         if os.path.isdir(destination_path):
             shutil.rmtree(destination_path)
