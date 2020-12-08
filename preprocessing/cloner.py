4738dc9d79a580d1c12ffdf1dcf578b85db59ec5
León Orell Valerian Liehr <liehr.exchange@gmx.net>
2020-12-08 22:24:55 +0100

    Clone repos to different path

--- preprocessing/cloner.py
+++ preprocessing/cloner.py
@@ -2,16 +2,21 @@ from git import Repo
 import os
 import shutil
 
+PROJECTS_DIR = "./projects"
+
+if not os.path.isdir(PROJECTS_DIR):
+    os.mkdir(PROJECTS_DIR)
+
 
 class Cloner:
-    def clone(self, author: str, repo: str, destination: str = '../projects') -> str:
+    def clone(self, author: str, repo: str, destination: str = PROJECTS_DIR) -> str:
         """
         Clone projects from GitHub
 
         If the project already exists at the destination location, we replace it by the new clone
         """
-        repo_url = 'git@github.com:' + author + '/' + repo + '.git'
-        destination_path = os.path.join(destination, author + '__' + repo)
+        repo_url = "git@github.com:" + author + "/" + repo + ".git"
+        destination_path = os.path.join(destination, author + "__" + repo)
 
         if os.path.isdir(destination_path):
             shutil.rmtree(destination_path)
